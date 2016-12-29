4b3826bf498c30389cbc0991fe230d9abd9e6b97
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-12-29 14:57:41 +0900

    Prog / D-5 3#1 / Processing / 20161229_122907
    steps=2
    -----
    1. gen --> movie.
    
    	==> done.
    
    ------- TEMPLATE
    1.  --> .
    
    	==> .

--- d.bat
+++ d.bat
@@ -1,6 +1,18 @@
 @ECHO OFF
 
-pushd C:\WORKS\Utils\shortcuts\prog_processing
-main.stop.bat
+taskkill /f /im wish.exe
+
+taskkill /f /im MovieMaker.exe
+
+taskkill /f /im audacity.exe
+
+taskkill /f /im wmplayer.exe
+
+taskkill /f /im eclipse.exe
+
+taskkill /f /im javaw.exe
+
+REM taskkill /f /im blender.exe
+REM taskkill /f /im blender-app.exe
 
 exit
